434f3d1a93f8cba8c54e268e90d0824954c7deb7
Jon Hurst <jon.a@hursts.org.uk>
2020-05-21 17:18:19 +0100

    New version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aimstool",
-    version="0.0.3",
+    version="0.0.4",
     author="Jon Hurst",
     author_email="jon.a@hursts.org.uk",
     description="A tool leveraging aimslib",
