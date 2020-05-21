351a8e34db82001ee01a6796f6f8a1cc0f538b3d
Jon Hurst <jon.a@hursts.org.uk>
2020-05-21 14:30:41 +0100

    Moved expanded dutylist creation and crewmap creation to seperate module in preparation for gui

--- aimstool/main.py
+++ aimstool/main.py
@@ -3,64 +3,23 @@ import sys
 import argparse
 import requests
 from getpass import getpass
-from typing import List
-import os.path
 
 import aimslib.access.connect
-from  aimslib.access.cache import TripCache, CrewlistCache
-import aimslib.access.brief_roster as Roster
-from aimslib.common.types import Duty, NoTripDetails, AIMSException
+from aimslib.common.types import AIMSException
 import aimslib.detailed_roster.process as dr
 
 from .freeform import build_freeform
 from .roster import roster
 from .ical import ical
 from .build_csv import build_csv
+from . import access
 
-CACHE_DIR = os.path.expanduser("~/.cache/")
 ECREW_LOGIN_PAGE = "https://ecrew.easyjet.com/wtouch/wtouch.exe/verify"
 
 
 def _heartbeat():
     sys.stderr.write('.')
     sys.stderr.flush()
-
-
-def _build_expanded_dutylist(post_func, months: int) -> List[Duty]:
-    sparse_dutylist = []
-    if months < 0: months += 1
-    else: months -= 1
-    for r in Roster.retrieve(post_func, months):
-        sparse_dutylist.extend(Roster.duties(Roster.parse(r)))
-    expanded_dutylist = []
-    trip_cache = TripCache(CACHE_DIR + "aimslib.tripcache", post_func)
-    last_id = None
-    for duty in sorted(sparse_dutylist):
-        if duty.trip_id == last_id: continue #avoid duplicates
-        last_id = duty.trip_id
-        if duty.start is None:
-            try:
-                expanded_dutylist.extend(trip_cache.trip(duty.trip_id))
-            except NoTripDetails:
-                print(f"Trip details not found for: {duty.trip_id}",
-                      file=sys.stderr)
-        else:
-            expanded_dutylist.append(duty)
-    trip_cache.store()
-    return expanded_dutylist
-
-
-def _crewlist_map(post_func, dutylist):
-    crew_cache = CrewlistCache(CACHE_DIR + "aimslib.clcache", post_func)
-    crewlist_map = {}
-    for duty in dutylist:
-        if duty.sectors:
-            for sector in duty.sectors:
-                if not sector.crewlist_id: continue
-                crewlist = crew_cache.crewlist(sector.crewlist_id)
-                crewlist_map[sector.crewlist_id] = crewlist
-    crew_cache.store()
-    return crewlist_map
 
 
 def online(args) -> int:
@@ -89,18 +48,18 @@ def online(args) -> int:
             aimslib.access.connect.logout(post_func)
             return -1
         if args.format == "freeform":
-            dutylist = _build_expanded_dutylist(post_func, -args.months)
-            crewlist_map = _crewlist_map(post_func, dutylist)
+            dutylist = access.duties(post_func, -args.months)
+            crewlist_map = access.crew(post_func, dutylist)
             print(build_freeform(dutylist, crewlist_map))
         elif args.format == "roster":
-            dutylist = _build_expanded_dutylist(post_func, args.months)
+            dutylist = access.duties(post_func, args.months)
             print(roster(dutylist))
         elif args.format == "ical":
-            dutylist = _build_expanded_dutylist(post_func, args.months)
+            dutylist = access.duties(post_func, args.months)
             print(ical(dutylist))
         elif args.format == 'csv':
-            dutylist = _build_expanded_dutylist(post_func, -args.months)
-            crewlist_map = _crewlist_map(post_func, dutylist)
+            dutylist = access.duties(post_func, -args.months)
+            crewlist_map = access.crew(post_func, dutylist)
             print(build_csv(dutylist, crewlist_map, args.fo))
         aimslib.access.connect.logout(post_func)
         return 0
